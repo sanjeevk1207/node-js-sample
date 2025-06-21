7592ff19edb9cee3e69add7bff0e202451fda5b7
sanjeevk1207 <121536051+sanjeevk1207@users.noreply.github.com>
2025-06-21 11:44:00 +0530

    Update index.js
    Signed-off-by: sanjeevk1207 <121536051+sanjeevk1207@users.noreply.github.com>

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ app.get('/', function(request, response) {
   response.send('Hello World!')
 })
 
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
+app.listen(app.get('port'), '0.0.0.0', function() {
+  console.log("Node app is running at 0.0.0.0:" + app.get('port'))
 })
